5d442527fe666a046bfeb54cf9a4fb2ad1b768c0
Carlos Loth <carlos_loth@live.com>
2025-07-24 10:50:33 -0300

    fixed prisma issue of missing monorep dependency

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
+// @ts-ignore
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -9,7 +12,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
       },
     ]
-  }
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
 };
 
 export default nextConfig;
